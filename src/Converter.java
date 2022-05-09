1c3467723f8b5a6ea86368a1ae440b8215ebe8e9
Stonewall1 <impression97p@gmail.com>
2022-05-09 18:35:20 +0300

    Reverse Converter V.2

--- src/Converter.java
+++ src/Converter.java
@@ -2,54 +2,42 @@
 import java.util.Scanner;
 
 public class Converter {
-    private static final String CS = "0123456789";
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
         System.out.println("Enter decimal number to convert");
         long decimalNumber = scanner.nextLong();
         System.out.println("Enter which system you would like to use(from 2)");
         int numberSystem = scanner.nextInt();
+
         if (numberSystem < 2) {
             System.out.println("Enter from 2");
         } else {System.out.println("Your number : " + ConvertToAny(decimalNumber, numberSystem));}
 
-        System.out.println("Enter new number in chosen system");
-        long newNumber = scanner.nextLong();
-        System.out.println(newNumber + " in " + numberSystem + " system = " + ConvertToDecimal(newNumber , numberSystem) + " in decimal system");
+        String newNumber = ConvertToAny(decimalNumber, numberSystem);
+        System.out.println("Reverse : " + ConvertToDecimal(newNumber , numberSystem));
     }
 
-    private static String ConvertToAny(long decimalNumber, int numberSystem) { // 43 ,25
-        ///////////////////////////
+    private static String ConvertToAny(long decimalNumber, int numberSystem) {
         String result = "";
         if (decimalNumber > 0) {
             while (decimalNumber != 0) {
                 int n = (int) (decimalNumber % numberSystem);
-                result = n + result;
+                result = n + "." + result;
                 decimalNumber = decimalNumber / numberSystem;
             }
-            return result; // 118
-            ///////////////////////////
-        } else if (decimalNumber == 0) {
-            return "0";
-        } else if (decimalNumber < 0) {
-            while (decimalNumber != 0) {
-                int n = -(int) (decimalNumber % numberSystem);
-                result = n + result;
-                decimalNumber = decimalNumber / numberSystem;
-            }
-            return "-" + result;
-        } else return "idk what happened";
+            return result; // 1.18.
+        }
+        else return "idk what happened";
     }
 
-    private static int ConvertToDecimal(long newNumber , int numberSystem) { // 118 , 25
-            String number = newNumber + "";
+    private static int ConvertToDecimal(String newNumber , int numberSystem) {
             int decimal = 0;
-            for (int i = 0; i < number.length(); i++) {
-                char c = number.charAt(i); //1 , 1 , 8
-                int index = CS.indexOf(c);//1 , 1 , 8
-                decimal = (decimal * numberSystem) + index;// (0*25)+1 = 1; (1*25)+1 = 26; (26*25)+8 = 658;
-            }
-            return decimal;//658
+            String[] array = newNumber.split("\\.");
+        for (int i = 0; i < array.length; i++) {
+            decimal = (numberSystem * decimal) + Integer.parseInt(array[i]);
+        }
+            return decimal;
         }
 }
 
